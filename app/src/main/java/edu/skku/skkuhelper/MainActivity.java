7c00a4b6120e997269826bcf6d17b74ff995ab7c
Brian-Hwang <brian11hwangb@gmail.com>
2022-10-31 10:04:29 +0900

    base notification + environment/dependencies built

--- app/src/main/java/edu/skku/skkuhelper/MainActivity.java
+++ app/src/main/java/edu/skku/skkuhelper/MainActivity.java
@@ -1,6 +1,10 @@
 package edu.skku.skkuhelper;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -9,6 +13,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
 
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -40,6 +46,22 @@ public class MainActivity extends AppCompatActivity {
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
+        /************* Channel creation for Notification *************/
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(String.valueOf(R.string.CHANNEL_ID), name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+        /************* Channel creation for Notification *************/
+
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -66,6 +88,28 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         /************* Example of Background Service *************/
+
+        /************* Notification builder creation for Notification *************/
+        // Create an explicit intent for an Activity in your app
+        //TODO change Intent activity to class specificaiton
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, String.valueOf(R.string.CHANNEL_ID))
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle("ICAMPUS NOTIFICATION")
+                .setContentText("Software Engineering lectrue due 10.03")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE);
+        /************* Notification builder creation for Notification *************/
+
+
+        /************* Example of Notification *************/
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        // notificationId is a unique int for each notification that you must define
+        notificationManager.notify(001, builder.build());
+        /************* Example of Notification *************/
 
     }
 
