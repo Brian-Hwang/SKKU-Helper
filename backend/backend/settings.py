c1dd376a5627a829a8f940d3d3c00d972c092b0c
Taek-Lee00 <dlrlxor548@gmail.com>
2022-11-15 03:20:20 +0900

    fix host ip

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -41,7 +41,7 @@ SECRET_KEY = get_secret("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['13.124.68.141']
 
 
 # Application definition
