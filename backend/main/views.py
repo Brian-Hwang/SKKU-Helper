e90442c24472504d725037ba8080db69c78cd3c0
Taek-Lee00 <dlrlxor548@gmail.com>
2022-11-18 22:51:14 +0900

    fix

--- backend/main/views.py
+++ backend/main/views.py
@@ -11,11 +11,11 @@ from .crawler import stop_background_process
 # Create your views here.
 
 client = MongoClient(
-        host='13.124.68.141', # aws 연동할때 마다 가져오기
-        port = 27017,
-        username = 'se',
-        password = '1234'
-    )
+    host='13.124.68.141',  # aws 연동할때 마다 가져오기
+    port=27017,
+    username='se',
+    password='1234'
+)
 
 
 @api_view(['GET', 'POST'])
@@ -23,21 +23,22 @@ def update_notice(request):
     if request.method == 'GET':
         title = request.GET['title']
         summarize = request.GET['summarize']
-        tag  = request.GET['tag']
-        writer  = request.GET['writer']
+        tag = request.GET['tag']
+        writer = request.GET['writer']
         date = request.GET['date']
-        link  = request.GET['link']
+        link = request.GET['link']
 
         db = client.noticeDB
         notice_collection = db.notice
         data = {}
         post_id = notice_collection.insert_many(data)
-        
+
         if result == []:
-            return JsonResponse({ "Result" : post_id})
+            return JsonResponse({"Result": post_id})
         else:
             return JsonResponse({"Result": False})
-        
+
+
 @api_view(['GET', 'POST'])
 def get_notice(request):
     if request.method == 'GET':
@@ -46,81 +47,88 @@ def get_notice(request):
         tag = int(tag)
         type = request.GET['type']
         type = int(type)
-        
-        print(int(tag))
-        print(tag)
-        
+
         result = []
-        
+
         db = client.noticeDB
         notice_collection = db.notice_crawling
         student_collection = db.student
-        new_notice = notice_collection.find({'id_server_notice' : {'$gt' : 800}}).sort('id_server_notice',-1).limit(1)
+        new_notice = notice_collection.find(
+            {'id_server_notice': {'$gt': 800}}).sort('id_server_notice', -1).limit(1)
         new_notice = list(new_notice)
         new_notice = new_notice[0]
         new_notice = new_notice['id_server_notice']
-        
-        check_student = student_collection.find({"student_id" : sid}, {"_id":0})
+
+        check_student = student_collection.find(
+            {"student_id": sid}, {"_id": 0})
         check_student = list(check_student)
         last_notice = new_notice
-        
+
         if check_student != []:
             print("check student in")
             check_student = check_student[0]
             last_notice = check_student["last_server_id"]
-            student_collection.update_one({'student_id' : sid}, {'$set' : {"last_server_id" : last_notice} } )
-        else :
+            student_collection.update_one(
+                {'student_id': sid}, {'$set': {"last_server_id": last_notice}})
+        else:
             new_student = {}
             new_student['student_id'] = sid
             new_student['last_server_id'] = new_notice
             student_collection.insert(new_student)
 
-        if type == 0: #tag별 + 최신 업데이트 100개
-            id = notice_collection.find({"tag_num" : tag} , {"_id" : 0, "id_homepage_notice" : 0}).sort("id_server_notice",-1).limit(100)
+        if type == 0:  # tag별 + 최신 업데이트 100개
+            id = notice_collection.find({"tag_num": tag}, {
+                                        "_id": 0, "id_homepage_notice": 0}).sort("id_server_notice", -1).limit(100)
             result = list(id)[:100]
-        if type == 1: #tag별 + 최신 업데이트 100개 중 조회수 순
-            id = notice_collection.find({"tag_num" : tag} , {"_id" : 0, "id_homepage_notice" : 0}).sort("id_server_notice",-1).limit(100).sort("watch",-1)
+        if type == 1:  # tag별 + 최신 업데이트 100개 중 조회수 순
+            id = notice_collection.find({"tag_num": tag}, {"_id": 0, "id_homepage_notice": 0}).sort(
+                "id_server_notice", -1).limit(100).sort("watch", -1)
             result = list(id)[:100]
-        if type == 2: #tag 상관 없이 + 최신 업데이트 100개
-            id = notice_collection.find({"tag_num" : {'$lt': 10}}, {"_id" : 0, "id_homepage_notice" : 0}).sort("id_server_notice",-1).limit(100)
+        if type == 2:  # tag 상관 없이 + 최신 업데이트 100개
+            id = notice_collection.find({"tag_num": {'$lt': 10}}, {
+                                        "_id": 0, "id_homepage_notice": 0}).sort("id_server_notice", -1).limit(100)
             result = list(id)[:100]
-        if type == 3: #tag 상관 없이 +최신 업데이트 100개 중 조회수 순
-            id = notice_collection.find({"tag_num" : {'$lt': 10}} , {"_id" : 0, "id_homepage_notice" : 0}).sort("id_server_notice",-1).limit(100).sort("watch",-1)
+        if type == 3:  # tag 상관 없이 +최신 업데이트 100개 중 조회수 순
+            id = notice_collection.find({"tag_num": {'$lt': 10}}, {"_id": 0, "id_homepage_notice": 0}).sort(
+                "id_server_notice", -1).limit(100).sort("watch", -1)
             result = list(id)[:100]
-        
+
         if result == []:
-            return JsonResponse({ "data" : "None"})
+            return JsonResponse({"data": "None"})
         else:
-            return JsonResponse({"data" : result})
-        
-        
+            return JsonResponse({"data": result})
+
+
 def test_input(request):
     if request.method == 'GET':
         title = request.GET['title']
         summarize = request.GET['summarize']
-        tag  = request.GET['tag']
-        writer  = request.GET['writer']
+        tag = request.GET['tag']
+        writer = request.GET['writer']
         date = request.GET['date']
-        link  = request.GET['link']
+        link = request.GET['link']
 
         tag = int(tag)
-        
+
         db = client.noticeDB
         notice_collection = db.notice
-        data = {'title' : title, 'summarize' : summarize, "tag" : tag, "writer" : writer, "date" : date, "link" : link}
+        data = {'title': title, 'summarize': summarize,
+                "tag": tag, "writer": writer, "date": date, "link": link}
         print(data)
         post_id = notice_collection.insert_one(data)
-        
+
         if post_id:
-            return JsonResponse({"Result" : True})
+            return JsonResponse({"Result": True})
         else:
             return JsonResponse({"Result": False})
+
 
 @api_view(['GET', 'POST'])
 def start_background(request):
     if request.method == 'GET':
         start_background_process()
         return JsonResponse({"Result": True})
+
 
 @api_view(['GET', 'POST'])
 def stop_background(request):
